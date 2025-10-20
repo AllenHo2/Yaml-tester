235bb2bab8f0f22e1d72178cf19c531524b90152
AllenHo2 <aho22@dons.usfca.edu>
2025-10-19 23:07:24 -0700

    Deployment works using npm run deploy

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {
-  output: "standalone", // Required for OpenNext
-};
+const config = {};
 
 export default config;
