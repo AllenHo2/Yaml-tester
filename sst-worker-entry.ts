6eee571d51c0d174a7d32baeb36b24542be648d1
AllenHo2 <aho22@dons.usfca.edu>
2025-12-01 13:56:04 -0800

    Remove sst

--- sst-worker-entry.ts
+++
@@ -1,7 +0,0 @@
-// This entry forwards to the OpenNext-generated worker
-// We use a relative path at runtime; SST bundles this file.
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import worker from "./.open-next/worker.js";
-
-export default worker;
